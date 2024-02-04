4e6aa0d1106d5ed511bce2d97cac1a5e039cc174
ClaytonMalarkey <malarkey1985@gmail.com>
2024-02-03 20:48:03 -0700

    more to add

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def create_client():
   username  = request.json.get('username')
   password  = request.json.get('password')
 
+  cursor = cnx.cursor(buffered=True)
   result = run_statement('CALL create_client(?,?,?,?,?,?)', [email, password, first_name, last_name, image_url, username ])
 
   return make_response(jsonify(result), 200)
@@ -94,19 +95,19 @@ def create_restaurant():
  try:
   
   # This is how you get values from body
-  name = request.json.get('name')
-  email  = request.json.get('email')
-  password  = request.json.get('password')
-  is_Verified = request.json.get('is_Verified')
-  verification_token = request.json.get('verifcation_token')
-  image_url  = request.json.get('image_url')
-  banner_url  = request.json.get('banner_iurl')
+   name = request.json.get('name')
+   email  = request.json.get('email')
+   password  = request.json.get('password')
+   is_Verified = request.json.get('is_Verified')
+   verification_token = request.json.get('verifcation_token')
+   image_url = request.json.get('image_url')
+   banner_url = request.json.get('banner_url')
 
-  result = run_statement('CALL create_restaurant(?,?,?,?,?,?,?)', [name, email, password, is_Verified, verification_token, image_url, banner_url])
+   result = run_statement('CALL create_restaurant(?,?,?,?,?,?,?)', [name, email, password, is_Verified, verification_token, image_url, banner_url])
 
-  return make_response(jsonify(result), 200)
+   return make_response(jsonify(result), 200)
  except:
-  return make_response(jsonify("Error"), 400)
+   return make_response(jsonify("Error"), 400)
 
 
 app.run(debug=True)
